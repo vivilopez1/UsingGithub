73c731458885f65175f33140c20afb52f81a2ca8
VivianaLopez2 <>
2023-03-28 19:30:55 +0000

    first commit

---
+++ .ccls-cache/@home@runner@UsingGithub/211-sp23-github-collab@collab.c
@@ -0,0 +1,95 @@
+// Collaborative code file, Lab 11
+//
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this
+// code!
+//
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+//
+// An example is shown below.
+//
+// UIC CS 211, Spring 2023
+#include <stdio.h>
+
+void reed_ekidan2() {
+  // Adding comment here
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void shadd3_alojko2() {
+  printf("Sammy Haddad and Angela Lojko\n");
+  printf("From the readings, we learned that git and GitHub are really effective when working on large projects with many people.\n");
+  printf("We can also see what a user changed/added to the program.\n"); 
+}
+
+void ppacho2_grohr2() {
+  puts("Peter Pacholarz and Gavin Rohrer");
+  puts("I learned that you need to make sure the polkit agent is running to 
+       "clone over ssh\n");
+  puts("I also learned that using git is easier than copy pasting to Google "
+       "Drive\n");
+  puts(
+      "Especially when backing up / copying the projects between computers.\n");
+}
+
+void mausaf2_kperez39() {
+  printf("We learned that git push uplaods local changes to a remote "
+         "repository\n");
+}
+
+void dkhar3_nvaly2() {
+  printf("Dev Kharvari and Nathan Valyaneth\n");
+  printf("We learned that GitHub is a collaborative cloud environment that "
+         "makes collaborating on projects really easy and simple");
+}
+void amari9_asurka2() {
+  printf("Aaryan Mariselvam and Aarav Surkatha");
+  printf("We learned there are many ways we can use and maipulate a git hub "
+         "repository file\n");
+}
+
+void stinu2_cdoma2() {
+  printf("Stephen and Conrad");
+  printf(
+      "I learned about the need for version control, especially when working "
+      "with large codebases. Git and GitHub, in general, allow multiple "
+      "developers to collaborate efficiently while keep code organized.");
+  printf("We learned about the versatile functionalities of git and how we are "
+         "able to keep version and logs of a working tree/ code progress");
+}
+
+
+void jgongo2_pkoby2_kseki3() {
+    printf("Juli Gongora Falla, Phillip Kobylinski, and Kaito Sekiya\n");
+    printf("We learned that commit and push are seperate operations. "
+           "You can make multiple commmits before pushing to the repository.\n");
+}
+
+void vlope22_rmirz2(){
+  printf("Viviana Lopez and Ruba Mirza\n");
+  printf("We learned that the git diff function displays all the changes between the different commits. Another thing is to include HEAD after git diff in order to compare changes with the most recent commit.");
+  printf("We also learned that when you run git push, it will send all of your local commits to the remote repository on Github. This makes changes visible to other collaborators.");
+}
+
+
+int main() {
+  printf(
+      "This code has been created by the students of CS 211, Spring 2023!\n\n");
+
+  reed_ekidan2();
+  shadd3_alojko2();
+  mausaf2_kperez39();
+  ppacho2_grohr2();
+  dkhar3_nvaly2();
+  amari9_asurka2();
+  jgongo2_pkoby2_kseki3();
+
+  return 0;
+}
